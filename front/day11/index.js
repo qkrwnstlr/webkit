0781d7a1ba0f8c65069bbd4932110e476a6ba3d0
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-09 09:11:00 +0900

    day11 : formiable, jquery

---
+++ front/day11/index.js
@@ -0,0 +1,74 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// npm install --save formidable
+// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=scw0531&logNo=221159117976
+const formidable = require('formidable'); // form-data 형식 통신(텍스트 외의 파일을 업로드)을 위한 모듈 
+const fs = require('fs'); // 파일 입출력
+
+
+// app.set("key", "value")  - setAttribute 용도로 사용
+// app.get("key");  - getAttribute의 용도로 사용
+// app.get("path", 콜백함수)  - 서버의 doGet 역할
+// app.post("path", 콜백함수)  - 서버의 doPost 역할
+app.set("port", 3000);
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/template");
+
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+	res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+	res.write("<h1>Hello world</h1>");
+	res.end();
+});
+
+// app.post("/saram/input", (req, res) => {
+// 	var form = new formidable.IncomingForm();
+// 	form.parse(req, function (err, fields, files) {
+// 		var oldpath = files.photo.filepath; // photo는 form 태그 내 input의 name
+// 		var newpath = './upload/' + files.photo.originalFilename; // 폴더명 뒤에는 항상 /로 마무리
+// 		fs.rename(oldpath, newpath, function (err) {
+// 			if (err) throw err;
+// 			console.log(newpath);
+// 			// res.write('File uploaded and moved!');
+// 			// res.end();
+// 			return res.redirect('/');
+// 		});
+// 	});
+// });
+
+app.post("/saram/input", (req, res) => {
+	var form = new formidable.IncomingForm();
+	console.log("start");
+
+	form.parse(req, function (err, fields, files) { // fields : 파일을 제외한 다른 값들, files : 파일들
+		console.log("parse");
+		res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+		res.write("<h2>upload ok!</h2>");
+		res.end();
+	});
+
+	form.on("end", function () { // end : 파일을 모두 전달 받은 후 수행되는 부분
+		console.log("end");
+		console.log("파일 갯수 : ", this.openedFiles.length);
+		for (var i = 0; i < this.openedFiles.length; i++) {
+			let file = this.openedFiles[i];
+			var oldpath = file.filepath;
+			var newpath = './upload/' + file.originalFilename;
+			fs.rename(oldpath, newpath, function (err) {
+				if (err) throw err;
+			});
+		}
+	});
+});
+
+
+// http와 express의 결합 - 같은 port를 공유한다.
+const server = http.createServer(app);
+server.listen(app.get("port"), () => {
+	console.log("서버 실행 중 - http://localhost:" + app.get("port"));
+});
