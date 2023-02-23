acfb0235cc834de747382c60eaa5053e2e1ef77b
qkrwnstlr <akfldkskgorn@gmail.com>
2023-02-23 21:07:11 +0900

    day19 : spring init & controller

---
+++ back/spring/sprintTest/src/main/java/com/example/sprinttest/SprintTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SprintTestApplication.class, args);
+  }
+
+}
---
+++ back/spring/sprintTest/src/test/java/com/example/sprinttest/SprintTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
